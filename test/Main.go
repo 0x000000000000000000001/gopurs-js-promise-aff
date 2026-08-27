8542331100bff9570a1355e07d6ef8b07291a0b0
0x1 <kevin.francart3@gmail.com>
2026-08-27 21:22:05 +0200

    misc

--- test/Main.go
+++ test/Main.go
@@ -2,11 +2,10 @@ package Test_Main
 
 import (
 	"errors"
-	"gopurs/output/Promise.Internal"
 	"gopurs/output/gopurs_runtime"
 )
 
-var HelloPromise = Promise_Internal.Resolve(gopurs_runtime.Box("Hello"))
-var GoodbyePromise = Promise_Internal.Reject(gopurs_runtime.Box("Goodbye"))
-var ErrPromise = Promise_Internal.Reject(gopurs_runtime.Box(errors.New("err")))
-var CustomErrPromise = Promise_Internal.Reject(gopurs_runtime.Box(map[string]interface{}{"code": "err"}))
+var HelloPromise = Promise_Internal_Resolve(gopurs_runtime.Box("Hello"))
+var GoodbyePromise = Promise_Internal_Reject(gopurs_runtime.Box("Goodbye"))
+var ErrPromise = Promise_Internal_Reject(gopurs_runtime.Box(errors.New("err")))
+var CustomErrPromise = Promise_Internal_Reject(gopurs_runtime.Box(map[string]interface{}{"code": "err"}))
